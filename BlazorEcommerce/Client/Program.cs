c4590ee622b8c8a39a7daa5e46c820005a39dd07
Hernan Provoste <hernanprovostev@outlook.com>
2023-04-15 13:58:26 -0400

    Finished GetProducts With Interfaces

--- BlazorEcommerce/Client/Program.cs
+++ BlazorEcommerce/Client/Program.cs
@@ -1,4 +1,6 @@
+global using BlazorEcommerce.Shared;
 using BlazorEcommerce.Client;
+using BlazorEcommerce.Client.Services.ProductService;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using MudBlazor.Services;
@@ -8,6 +10,7 @@ builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddScoped<IProductService, ProductService>();
 builder.Services.AddMudServices();
 
 await builder.Build().RunAsync();
